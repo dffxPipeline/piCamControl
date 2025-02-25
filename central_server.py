6a214eb001e154cfc9c5029ceda13b3e71576455
Chad Finnerty <cfinnerty@digitalfrontierfx.com>
2025-02-25 13:33:03 -0800

    Added Multiple Camera Streams

---
+++ central_server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# List of Raspberry Pi IP addresses
+raspberry_pi_ips = [
+    "192.168.1.101",
+    "192.168.1.102",
+    # Add all other IP addresses here
+]
+
+@app.route('/')
+def index():
+    """Render HTML page with all camera feeds."""
+    return render_template('index.html', raspberry_pi_ips=raspberry_pi_ips)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
