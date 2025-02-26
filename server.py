c6f75dbff0867d62ff578b59490f0f84a6f36646
Chad Finnerty <cfinnerty@digitalfrontierfx.com>
2025-02-26 03:43:25 -0800

    fixed .encoders and .outputs for picamera2

--- server.py
+++ server.py
@@ -44,8 +44,8 @@ from flask import Flask, render_template, request, jsonify, Response
 from adafruit_servokit import ServoKit
 import cv2
 from picamera2 import Picamera2
-from picamera2 import H264Encoder
-from picamera2 import FfmpegOutput
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput
 import board
 import busio
 from adafruit_pca9685 import PCA9685
