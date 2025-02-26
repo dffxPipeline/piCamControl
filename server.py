81780459245ee80ff8eb59e9c33c0a3ee9f9a3b4
Chad Finnerty <cfinnerty@digitalfrontierfx.com>
2025-02-26 12:04:39 -0800

    added back and adjusted encoder

--- server.py
+++ server.py
@@ -173,9 +173,8 @@ def record():
                 # Start the recording process using Picamera2
                 print("Starting video recording...")
                 video_output = FfmpegOutput("video.h264")
-                #encoder = H264Encoder(10000000)
-                #picam2.start_recording(encoder,output=video_output)
-                picam2.start_recording(output=video_output)
+                encoder = H264Encoder(name='h264', bitrate=10000000, framerate=30)
+                picam2.start_recording(encoder,output=video_output)
                 #picam2.start_recording(cv2.VideoWriter("video.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 30, (1280, 720)))
                 recording_process = True
                 print("Recording started successfully.")
