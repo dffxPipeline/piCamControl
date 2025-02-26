ae16b0513d11a2b0ea1f31252953520113d7bd3f
Chad Finnerty <cfinnerty@digitalfrontierfx.com>
2025-02-26 12:17:15 -0800

    removed encoder args

--- server.py
+++ server.py
@@ -173,7 +173,7 @@ def record():
                 # Start the recording process using Picamera2
                 print("Starting video recording...")
                 video_output = FfmpegOutput("video.h264")
-                encoder = H264Encoder(bitrate=10000000, framerate=30)
+                encoder = H264Encoder()
                 picam2.start_recording(encoder,output=video_output)
                 #picam2.start_recording(cv2.VideoWriter("video.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 30, (1280, 720)))
                 recording_process = True
